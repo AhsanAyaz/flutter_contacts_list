cfc79c79411fe5015cb61c62a2e7da33945a781d
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-04-12 02:17:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# contacts_app
+
+A new Flutter project to demonstrate how to implement a Contacts List via `contacts_service` package and how to search within that list using name or number.
+
+<div style="text-align: center">
+  <a href="https://youtu.be/NBIF-Wt_ZFQ" target="_blank">
+    <img src="assets/cover.png">
+  </a>
+</div>
+
+## Video Tutorial
+The video tutorial for this repo can be found at the link below:
+[https://youtu.be/NBIF-Wt_ZFQ](https://youtu.be/NBIF-Wt_ZFQ)
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Contacts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Contact> contacts = [];
+  List<Contact> contactsFiltered = [];
+  TextEditingController searchController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getAllContacts();
+    searchController.addListener(() {
+      filterContacts();
+    });
+  }
+
+  String flattenPhoneNumber(String phoneStr) {
+    return phoneStr.replaceAllMapped(RegExp(r'^(\+)|\D'), (Match m) {
+      return m[0] == "+" ? "+" : "";
+    });
+  }
+
+  getAllContacts() async {
+    List<Contact> _contacts = (await ContactsService.getContacts()).toList();
+    setState(() {
+      contacts = _contacts;
+    });
+  }
+
+  filterContacts() {
+    List<Contact> _contacts = [];
+    _contacts.addAll(contacts);
+    if (searchController.text.isNotEmpty) {
+      _contacts.retainWhere((contact) {
+        String searchTerm = searchController.text.toLowerCase();
+        String searchTermFlatten = flattenPhoneNumber(searchTerm);
+        String contactName = contact.displayName.toLowerCase();
+        bool nameMatches = contactName.contains(searchTerm);
+        if (nameMatches == true) {
+          return true;
+        }
+
+        if (searchTermFlatten.isEmpty) {
+          return false;
+        }
+
+        var phone = contact.phones.firstWhere((phn) {
+          String phnFlattened = flattenPhoneNumber(phn.value);
+          return phnFlattened.contains(searchTermFlatten);
+        }, orElse: () => null);
+
+        return phone != null;
+      });
+
+      setState(() {
+        contactsFiltered = _contacts;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSearching = searchController.text.isNotEmpty;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  border: new OutlineInputBorder(
+                    borderSide: new BorderSide(
+                      color: Theme.of(context).primaryColor
+                    )
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Theme.of(context).primaryColor
+                  )
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: isSearching == true ? contactsFiltered.length : contacts.length,
+                itemBuilder: (context, index) {
+                  Contact contact = isSearching == true ? contactsFiltered[index] : contacts[index];
+                  return ListTile(
+                    title: Text(contact.displayName),
+                    subtitle: Text(
+                      contact.phones.elementAt(0).value
+                    ),
+                    leading: (contact.avatar != null && contact.avatar.length > 0) ?
+                      CircleAvatar(
+                        backgroundImage: MemoryImage(contact.avatar),
+                      ) : 
+                      CircleAvatar(child: Text(contact.initials()))
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

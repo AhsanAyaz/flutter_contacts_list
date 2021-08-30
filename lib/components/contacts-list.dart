69ba70628fb7b9f2ed4cbec43766ceddba6f168f
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-08-30 15:15:59 +0200

    feat(contacts): edit, delete notes

--- lib/components/contacts-list.dart
+++ lib/components/contacts-list.dart
@@ -1,12 +1,13 @@
 import 'package:contacts_app/app-contact.class.dart';
+import 'package:contacts_app/pages/contact-details.dart';
 import 'package:flutter/material.dart';
 
 import 'contact-avatar.dart';
 
 class ContactsList extends StatelessWidget {
   final List<AppContact> contacts;
-
-  ContactsList({Key key, this.contacts}) : super(key: key);
+  Function() reloadContacts;
+  ContactsList({Key key, this.contacts, this.reloadContacts}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -17,11 +18,25 @@ class ContactsList extends StatelessWidget {
           AppContact contact = contacts[index];
 
           return ListTile(
-              title: Text(contact.info.displayName),
-              subtitle: Text(
-                  contact.info.phones.length > 0 ? contact.info.phones.elementAt(0).value : ''
-              ),
-              leading: ContactAvatar(contact, 36)
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => ContactDetails(
+                  contact,
+                  onContactDelete: (AppContact _contact) {
+                    reloadContacts();
+                    Navigator.of(context).pop();
+                  },
+                  onContactUpdate: (AppContact _contact) {
+                    reloadContacts();
+                  },
+                )
+              ));
+            },
+            title: Text(contact.info.displayName),
+            subtitle: Text(
+                contact.info.phones.length > 0 ? contact.info.phones.elementAt(0).value : ''
+            ),
+            leading: ContactAvatar(contact, 36)
           );
         },
       ),
--- lib/pages/contact-details.dart
+++ lib/pages/contact-details.dart
@@ -1,5 +1,6 @@
 import 'package:contacts_app/app-contact.class.dart';
 import 'package:contacts_app/components/contact-avatar.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 
 class ContactDetails extends StatefulWidget {
@@ -16,6 +17,70 @@ class _ContactDetailsState extends State<ContactDetails> {
 
   @override
   Widget build(BuildContext context) {
+    List<String> actions = <String>[
+      'Edit',
+      'Delete'
+    ];
+
+    showDeleteConfirmation() {
+      Widget cancelButton = FlatButton(
+        child: Text('Cancel'),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      );
+      Widget deleteButton = FlatButton(
+        color: Colors.red,
+        child: Text('Delete'),
+        onPressed: () async {
+          await ContactsService.deleteContact(widget.contact.info);
+          widget.onContactDelete(widget.contact);
+          Navigator.of(context).pop();
+        },
+      );
+      AlertDialog alert= AlertDialog(
+        title: Text('Delete contact?'),
+        content: Text('Are you sure you want to delete this contact?'),
+        actions: <Widget>[
+          cancelButton,
+          deleteButton
+        ],
+      );
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        }
+      );
+
+    }
+
+    onAction(String action) async {
+      switch(action) {
+        case 'Edit':
+          try {
+            Contact updatedContact = await ContactsService.openExistingContact(widget.contact.info);
+            setState(() {
+              widget.contact.info = updatedContact;
+            });
+            widget.onContactUpdate(widget.contact);
+          } on FormOperationException catch (e) {
+            switch(e.errorCode) {
+              case FormOperationErrorCode.FORM_OPERATION_CANCELED:
+              case FormOperationErrorCode.FORM_COULD_NOT_BE_OPEN:
+              case FormOperationErrorCode.FORM_OPERATION_UNKNOWN_ERROR:
+                print(e.toString());
+            }
+          }
+          break;
+        case 'Delete':
+          showDeleteConfirmation();
+          break;
+      }
+      print(action);
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -38,6 +103,23 @@ class _ContactDetailsState extends State<ContactDetails> {
                       ),
                     ),
                     alignment: Alignment.topLeft,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        onSelected: onAction,
+                        itemBuilder: (BuildContext context) {
+                          return actions.map((String action) {
+                            return PopupMenuItem(
+                              value: action,
+                              child: Text(action),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ),
                   )
                 ],
               ),
